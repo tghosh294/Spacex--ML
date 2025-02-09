d22b7bcd168ebac934edec1eb370e07a094af3ad
Tamasi Ghosh <143017377+tghosh294@users.noreply.github.com>
2025-02-09 16:23:57 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import warnings
+warnings.filterwarnings("ignore")
+
+# Load the dataset
+data = pd.read_csv('SpaceX_Falcon9.csv')
+
+# Handle missing values
+print(data.isnull().sum())
+
+data['PayloadMass'].fillna(data['PayloadMass'].mean(), inplace=True)
+data['LandingPad'].fillna('Unknown', inplace=True)
+
+print(data.isnull().sum())
+
+# Feature Engineering
+categorical_columns = ["Orbit", "LaunchSite", "LandingPad", "BoosterVersion", "Serial"]
+features = data[[
+    "FlightNumber", "PayloadMass", "Orbit", "LaunchSite", "Flights", 
+    "GridFins", "Reused", "Legs", "LandingPad", "Block", 
+    "ReusedCount", "Longitude", "Latitude", "BoosterVersion", "Serial"
+]]
+
+# One-hot encoding for categorical variables
+features_one_hot = pd.get_dummies(features, columns=categorical_columns, drop_first=True)
+print(features_one_hot)
+
+#check is there any null value present now
+print("\nMissing Values After One-Hot Encoding:")
+print(features_one_hot.isnull().sum().sum())
